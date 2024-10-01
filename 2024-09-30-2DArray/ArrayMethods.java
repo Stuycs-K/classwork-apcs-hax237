67b7182f13dbf731b9f2405795ff7549c76f2270
hax237 <han.xiao5627@gmail.com>
2024-09-30 21:50:25 -0400

    Need to add more test cases for handling empties.

--- 2024-09-30-2DArray/ArrayMethods.java
+++ 2024-09-30-2DArray/ArrayMethods.java
@@ -7,7 +7,23 @@
 * Note the comma+space between values, and between values
 */
 public class ArrayMethods{
-  public static String arrayToString(int[] nums){
+  public static void main(String[] args) {
+    int[] arr1D = new int[] {3,6,9,10};
+    int[][] arr2D = new int[][] {{3,5,6,7},{3,3,2},{5,4},{4}};
+    System.out.println(arrToString(arr1D));
+    //ragged array
+    System.out.println(arrToString(arr2D));
+    System.out.println(arr2DSum(arr2D));
+    System.out.println(arrToString(swapRC(new int[][]{{1,2,3},{4,5,6}}))); // should return {{1,4},{2,5},{3,6}}
+
+    arr2D = new int[][] {{3,5,6,7},{3,3,2,9},{5,2,1,4},{4, 10, 4, 7}};
+    System.out.println(arrToString(swapRC(arr2D)));
+    System.out.println(arrToString(arr2D));
+    System.out.println(arr2DSum(arr2D));
+
+  }
+
+  public static String arrToString(int[] nums){
     String ans = "[";
     for (int i = 0; i < nums.length; i++){
       ans = ans + nums[i];
@@ -26,23 +42,41 @@ public class ArrayMethods{
     * You are encouraged to notice that you may want to re-use
     * previous code, but you should NOT duplicate that code. (Don't copy/paste or retype it)
     */
-  public static String arrToString(int[][]ary){
-    //this should use arrToString(int[])
-    arrayToString(ary);
-    return "";
-  }
+    public static String arrToString(int[][]ary){
+      //this should use arrToString(int[])
+      String newArray = "[";
+      for (int i = 0; i < ary.length; i++){
+        newArray += arrToString(ary[i]);
+        if (i < ary.length - 1){
+          newArray += ", ";
+        }
+      }
+      return newArray + "]";
+    }
 
   /*Return the sum of all of the values in the 2D array */
   public static int arr2DSum(int[][]nums){
     //use a nested loop to solve this
-    return null;//place holder return value so it compiles.
+    int sum = 0;
+    for (int i = 0 ; i < nums.length; i++){
+      for (int x = 0; x < nums[i].length; x++){
+        sum += nums[i][x];
+      }
+    }
+    return sum;//place holder return value so it compiles.
   }
 
   /**Rotate an array by returning a new array with the rows and columns swapped.
     * You may assume the array is rectangular and neither rows nor cols is 0.
     * e.g. swapRC({{1,2,3},{4,5,6}}) returns {{1,4},{2,5},{3,6}}
     */
-  public static int[][] swapRC(int[][]nums){
-    return new int[1][1];
-  }
+    public static int[][] swapRC(int[][]nums){
+      int[][] arr = new int[nums[0].length][nums.length];
+      for (int i = 0; i < nums.length; i++){
+        for (int x = 0; x < nums[0].length; x++){
+          arr[x][i] = nums[i][x];
+        }
+      }
+      return arr;
+    }
 }
