7b4de0206ae187b76f76b332f565b6b7a141137a
hax237 <han.xiao5627@gmail.com>
2024-11-07 14:20:38 -0500

    transferred over files from in-class work

---
+++ 2024-11-07-Pets/Driver.java
@@ -0,0 +1,15 @@
+public class Driver{
+    public static void main(String[] args){
+      Animal a = new Animal("buzz",5,"bee");
+      a.speak();
+  
+      Bird b = new Bird("tweet", 2, "bob", 12.0, "brown");
+      b.speak();
+      System.out.println(b.getName() + " should return something with Mighty");
+      System.out.println(a.getName() + " should not return something with Mighty");
+    }
+  
+    //Step 8 answer
+    //calling getName from a bird object uses the overridden method of getName method. 
+    //However, calling getName from a Animal object uses the original getName method. 
+  }
