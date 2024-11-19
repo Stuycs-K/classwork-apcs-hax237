672ab7aea8f4eee9eef8ab5b0a42324c9bdc6ecc
hax237 <han.xiao5627@gmail.com>
2024-11-19 11:47:39 -0500

    made input file, need to compile and check studd

--- 2024-11-18-advent2016/adventOfCode1.java
+++ 2024-11-18-advent2016/adventOfCode1.java
@@ -3,6 +3,38 @@ import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class TriangleTester{
+public class adventOfCode1{
+
+  public static String[] parse(){
+    return null;
+  }
+
+  public static int blocksAway(String filename){
+    try{
+      File file = new File(filename);
+      Scanner input = new Scanner(file);
+      int i = 0, x = 0, y = 0;
+
+      /*
+      while(input.hasNext()){
+
+      }
+      System.out.println(xAxis);
+      System.out.println(yAxis);
+
+      */
+      System.out.println(file);
+      return 0;
+
+
+    }catch(FileNotFoundException e){
+      System.out.println("File not found");
+      return -100;
+    }
+  }
+
+  public static void main(String[] args){
+    System.out.println(blocksAway("input.txt"));
+  }
 
 }
