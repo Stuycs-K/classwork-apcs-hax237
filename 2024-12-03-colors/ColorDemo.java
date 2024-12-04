f3eb0861d36d10224d2b1268468189aa02b34fa4
hax237 <han.xiao5627@gmail.com>
2024-12-04 06:17:53 -0500

    tested all class features + made progress bar animation??

--- 2024-12-03-colors/ColorDemo.java
+++ 2024-12-03-colors/ColorDemo.java
@@ -1,5 +1,70 @@
 public class ColorDemo {
-  public static void main(String[] args) {
+  public static final String CLEAR_SCREEN =  "\u001b[2J";
+  public static final String HIDE_CURSOR =  "\u001b[?25l";
+  public static final String SHOW_CURSOR =  "\u001b[?25h";
+  public static int BLACK = 30;
+  public static int RED = 31;
 
+  public static void color(int foreground,int background){
+    System.out.print( "\u001b[" + foreground + ";" + (10+background) + "m");
+  }
+
+  public static void go(int r,int c){
+    System.out.print("\u001b[" + r + ";" + c + "f");
+  }
+
+  public static void color(int foreground, int background, int modifier){
+    System.out.print( "\u001b[" + foreground + ";" + (10+background) + ";" + modifier + "m");
+  }
+  public static void main(String[] args) {
+    System.out.print("\u001b[31m");
+    System.out.println("Hello World");
+    System.out.print("\u001b[0m");
+
+    //System.out.println('\n');
+
+    System.out.print("\u001b[31mRed");
+    System.out.print(" fish ");
+    System.out.print("\u001b[34mBlue");
+    System.out.print(" fish ");
+    System.out.print(" also blue because no reset... also your terminal is blue now too...");
+
+    System.out.print("\u001b[31mRed fish");
+    System.out.print(" \u001b[0mfish ");
+    System.out.print("\u001b[34mBlue");
+    System.out.print(" \u001b[0mfish ");
+
+    //System.out.print("\u001b[1;4;31;103m");
+    //System.out.print("bold underlined inverted");
+    //System.out.print(" \u001b[0m");
+
+
+    System.out.println('\n');
+    for(int r = 0; r < 256; r+=32){
+      for(int g = 0; g <= 256; g+=32){
+        for(int b = 0; b <= 256; b+=32){
+          System.out.print("\u001b[38;2;"+r+";"+g+";"+b+";7m.");
+        }
+      }
+        System.out.println();
+    }
+
+    System.out.print(HIDE_CURSOR);
+    color(RED, BLACK);
+    System.out.print("spider");
+    go(3, 5);
+    System.out.print("NOOOOOOO");
+
+    for (int i = 0; i <= 100; i++) {
+      go(38, 10);
+      System.out.print("Progress: " + i + "%");
+      try {
+          Thread.sleep(50); 
+      } catch (InterruptedException e) {
+          System.err.println("Thread was interrupted: " + e.getMessage());
+      }
+    }
+    //return to default
+    //System.out.print(" \u001b[0m ");
   }
 }
