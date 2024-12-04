9b349fcae74459cdc807b84bf232dab0195a75a9
hax237 <han.xiao5627@gmail.com>
2024-12-04 11:21:14 -0500

    added dirs.

--- 2024-12-04-ColorScreen/Driver.java
+++ 2024-12-04-ColorScreen/Driver.java
@@ -1,5 +1,18 @@
 public class Driver{
   public static void main(String[] args) {
+    /*
+    Create an 80x30 border of color (can be multiple, including different foreground/background) around your screen. It should be only 1 character thick.
+    Make an array of 3 random integers, range from 0-99 inclusive
+    Display the 3 integers evenly spaced (approximately) on the row below the top border.
+    Depending on the values of the integers, print them in the following colors:
+
+        under 25: bright red
+        over 75: bright green
+        otherwise: default to white (I am assuming your terminal background is black.)
+
+    On the line below your random numbers, draw a horizontal separator (you can match your border's color, but use a differnet character.
+    After you draw everything (including the next set of instructions), move your cursor to line 31 before your program exits, otherwise your terminal will draw over your screen.
+*/
     
   }
 
