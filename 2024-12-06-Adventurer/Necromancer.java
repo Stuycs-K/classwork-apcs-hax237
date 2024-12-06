bf8942d9aa8172e4be55a7254ba5f55258873e0a
hax237 <han.xiao5627@gmail.com>
2024-12-06 11:30:05 -0500

    made subclass, added new private variables and constructors

---
+++ 2024-12-06-Adventurer/Necromancer.java
@@ -0,0 +1,13 @@
+public class Necromancer extends Adventurer{
+  private String specialName;
+  private int special;
+  private int SpecialMax;
+
+  public Necromancer(String name){
+    super(name, hp);
+  }
+
+  public Necromancer(String name, int hp){
+    super(name, hp);
+  }
+}
