37f99ec03ee41a1ef6b663e47c797481ce1839ea
hax237 <han.xiao5627@gmail.com>
2024-12-06 21:04:34 -0500

    tested all subclass methods--they are working!

--- 2024-12-06-Adventurer/Driver.java
+++ 2024-12-06-Adventurer/Driver.java
@@ -1,5 +1,35 @@
 public class Driver{
   public static void main(String[] args) {
-    
+    Adventurer Michael = new StuyStudent("Michael", 100, 100);
+    Adventurer Chester = new StuyStudent("Chester", 100,100);
+    Adventurer Cassy = new StuyStudent("Cassy", 100,100);
+
+    System.out.println("\n--------Should All Be Caffeine--------\n");
+    System.out.println(Michael.getSpecialName());
+    System.out.println(Chester.getSpecialName());
+    System.out.println(Cassy.getSpecialName());
+
+    System.out.print("\n-------------Names--------------\n");
+    System.out.println(Michael.getName()+ " has " + Michael.getSpecial() + "mgs of caffeine available along with " + Michael.getHP() + "hp.");
+    System.out.println(Chester.getName() + " has " + Chester.getSpecial() + "mgs of caffeine available along with " + Chester.getHP() + "hp.");
+    System.out.println(Cassy.getName() + " has " + Cassy.getSpecial() + "mgs of caffeine available along with " + Cassy.getHP() + "hp.");
+
+    System.out.print("\n-------------FIGHT!--------------\n");
+    System.out.println(Michael.attack(Cassy));
+    System.out.println("Cassy now has "+Cassy.getHP() + "hp");
+    System.out.println(Cassy.specialAttack(Michael));
+    System.out.println("Michael now has "+Michael.getHP() + "hp");
+    System.out.println("Cassy now has "+Cassy.getSpecial() + "mgs of caffeine left");
+    System.out.println(Michael.specialAttack(Cassy));
+    System.out.println("Cassy now has "+Cassy.getHP() + "hp");
+    System.out.println(Chester.support(Cassy));
+    System.out.println("Cassy now has "+Cassy.getHP() + "hp");
+    System.out.println(Michael.support());
+    System.out.println(Chester.attack(Michael));
+    System.out.println("Michael now has "+Michael.getHP() + "hp");
+    System.out.println(Cassy.restoreSpecial(90));
+    System.out.println("Cassy now has "+Cassy.getSpecial() + "mgs of caffeine.");
+    System.out.println(Michael.support());
+    System.out.println(Michael.support());
   }
 }
--- 2024-12-06-Adventurer/StuyStudent.java
+++ 2024-12-06-Adventurer/StuyStudent.java
@@ -4,10 +4,14 @@ public class StuyStudent extends Adventurer{
 
   public StuyStudent(String name){
     super(name, 10);
+    caffeine = 100;
+    caffeineMax = 100;
   }
 
-  public StuyStudent(String name, int hp){
+  public StuyStudent(String name, int hp, int mg){
     super(name, hp);
+    caffeine = mg;
+    caffeineMax = mg;
   }
 
   public String getSpecialName(){
@@ -20,7 +24,6 @@ public class StuyStudent extends Adventurer{
 
   public void setSpecial(int n){
     this.caffeine = n;
-    this.caffeineMax = n;
   }
 
   public int getSpecialMax(){
@@ -29,23 +32,23 @@ public class StuyStudent extends Adventurer{
 
   public String attack(Adventurer other){
     other.applyDamage(10);
-    return "You've just distracted an adventurer from studying for their APCS exam!\n-10hp from " + other.getName();
+    return this.getName()+" just distracted " + other.getName()+" from studying for their APCS exam!\n-10hp from " + other.getName();
   }
 
   public String support(Adventurer other){
     other.setHP(other.getHP() + 7);
-    return "You just tutored an ally!\n+7hp for " + other.getName();
+    return this.getName()+" just tutored an ally!\n+7hp for " + other.getName();
   }
 
   public String support(){
     this.setHP(this.getHP() + 10);
-    return "*intense studying noises*\nYou've just healed for +10hp!";
+    return "*intense studying noises*\n" + this.getName() + " healed themselves for +10hp!";
   }
 
   public String specialAttack(Adventurer other){
     other.applyDamage(30);
     this.setSpecial(this.getSpecial() - 60);
-    return "You just chugged a celcius/coffee/monster energy! You ran circles around your opponent and sang baby shark off-key.\n-30hp from "+ other.getName() + "!";
+    return this.getName() + " just chugged a celcius/coffee/monster energy and ran circles around " + other.getName() + " while singing baby shark off-key.\n-30hp from "+ other.getName() + "!";
   }
 
 }
