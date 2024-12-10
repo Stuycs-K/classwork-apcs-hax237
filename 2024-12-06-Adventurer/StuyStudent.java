15437470faf44fa0b14298cfc9e0cba527dab36f
hax237 <han.xiao5627@gmail.com>
2024-12-09 21:29:58 -0500

    changed stuystudent class, fixed specialattack errors and cleaned up code. added constructors.

--- 2024-12-06-Adventurer/StuyStudent.java
+++ 2024-12-06-Adventurer/StuyStudent.java
@@ -1,17 +1,26 @@
 public class StuyStudent extends Adventurer{
   private int caffeine;
   private int caffeineMax;
+  private String bestSubject;
+  
 
-  public StuyStudent(String name){
-    super(name, 10);
+  public StuyStudent(String name, int hp, String sub){
+    super(name, hp);
     caffeine = 100;
     caffeineMax = 100;
+    bestSubject = sub;
   }
 
-  public StuyStudent(String name, int hp, int mg){
-    super(name, hp);
-    caffeine = mg;
-    caffeineMax = mg;
+  public StuyStudent(String name, int hp){
+    this(name, hp, "CS");
+  }
+  
+  public StuyStudent(String name){
+    this(name, 100);
+  }
+
+  public StuyStudent(){
+    this("Jimmy");
   }
 
   public String getSpecialName(){
@@ -30,25 +39,34 @@ public class StuyStudent extends Adventurer{
     return caffeineMax;
   }
 
+  public String getSubject(){
+    return bestSubject;
+  }
+
   public String attack(Adventurer other){
     other.applyDamage(10);
-    return this.getName()+" just distracted " + other.getName()+" from studying for their APCS exam!\n-10hp from " + other.getName();
+    return this+" just distracted " + other +" from studying for their "+ other.getSubject()+" exam!\n-10hp from " + other;
   }
 
   public String support(Adventurer other){
     other.setHP(other.getHP() + 7);
-    return this.getName()+" just tutored an ally!\n+7hp for " + other.getName();
+    return this+" just tutored an ally!\n+7hp for " + other.getName();
   }
 
   public String support(){
     this.setHP(this.getHP() + 10);
-    return "*intense studying noises*\n" + this.getName() + " healed themselves for +10hp!";
+    return "*intense studying noises*\n" + this + " healed themselves for +10hp!";
   }
 
   public String specialAttack(Adventurer other){
-    other.applyDamage(30);
-    this.setSpecial(this.getSpecial() - 60);
-    return this.getName() + " just chugged a celcius/coffee/monster energy and ran circles around " + other.getName() + " while singing baby shark off-key.\n-30hp from "+ other.getName() + "!";
+    if (this.getSpecial() > 60){
+      other.applyDamage(30);
+      this.setSpecial(this.getSpecial() - 60);
+      return this + " just chugged a Mr. Beast energy/celcius/coffee/monster energy and ran circles around " + other + " while singing baby shark off-key.\n-30hp from "+ other + "!";
+    }
+    else{
+      return this + " needs another red bull/coffee/celcius/rockstar/5-hour-energy drink!\nNot enough caffeine: " + this.getSpecial() + "mgs left :(";
+    }
   }
 
 }
